f2583af481f48698481eb984fcad9086a20ab3dd
Josecardoza12 <jos.cardoza@duocuc.cl>
2025-11-03 23:17:15 -0300

    Corrijo conexión final con PostgreSQL interno

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
 
-    # Ajuste automático para Render (PostgreSQL)
+    # Detectar la URL de Render
     uri = os.environ.get("DATABASE_URL")
     if uri and uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql+psycopg2://", 1)
+        uri = uri.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_DATABASE_URI = uri or f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
